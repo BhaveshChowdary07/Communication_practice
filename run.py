a8a01aaa1fec637c453507413b4b1c19be7e8032
BruKoffee <160404560+BhaveshChowdary07@users.noreply.github.com>
2025-05-24 15:25:25 +0530

    new load request queuing

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
+
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-    'pool_size': 10,
+    'pool_size': 15,
     'max_overflow': 20,
     'pool_timeout': 30,
-    'pool_recycle': 1800
+    'pool_recycle': 900,
+    'pool_pre_ping': True
 }
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gunicorn -k gevent -w 4 -b 0.0.0.0:10000 --backlog 512 run:app
+gunicorn -k gevent -w 4 --backlog 512 -t 60 --graceful-timeout 30 -b 0.0.0.0:10000 run:app
