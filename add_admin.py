0bd31c68ca1f1feab938f99c13f45db79e6a78d7
BruKoffee <160404560+BhaveshChowdary07@users.noreply.github.com>
2025-07-08 11:12:24 +0530

    new database connection

--- add_admin.py
+++ add_admin.py
@@ -1,16 +1,16 @@
 from app import app, db
-from app.models import Admin
+from app.models import User
 from werkzeug.security import generate_password_hash
 
 with app.app_context():
-    email = "ankurah777@gmail.com"
+    email = "bhavesh.chowdary@redsage.global"
     raw_password = "admin123"
-    existing = Admin.query.filter_by(email=email).first()
+    existing = User.query.filter_by(email=email, role='admin').first()
 
     if existing:
         print("⚠️ Admin already exists.")
     else:
-        admin = Admin(email=email, password=generate_password_hash(raw_password))
+        admin = User(email=email, password=generate_password_hash(raw_password), role='admin')
         db.session.add(admin)
         db.session.commit()
         print(f"✅ Admin added. Email: {email}, Password: {raw_password}")
