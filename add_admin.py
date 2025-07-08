5d9d4c4f17ebbdb1a676efb0a37aae4e6d46dbf2
BruKoffee <160404560+BhaveshChowdary07@users.noreply.github.com>
2025-07-08 12:40:41 +0530

    new pt icon

--- add_admin.py
+++ add_admin.py
@@ -1,16 +1,20 @@
 from app import app, db
 from app.models import User
 from werkzeug.security import generate_password_hash
-
 with app.app_context():
+    name = "Bhavesh Chowdary"
     email = "bhavesh.chowdary@redsage.global"
     raw_password = "admin123"
     existing = User.query.filter_by(email=email, role='admin').first()
-
     if existing:
         print("⚠️ Admin already exists.")
     else:
-        admin = User(email=email, password=generate_password_hash(raw_password), role='admin')
+        admin = User(
+            name=name,
+            email=email,
+            password=generate_password_hash(raw_password),
+            role='admin'
+        )
         db.session.add(admin)
         db.session.commit()
-        print(f"✅ Admin added. Email: {email}, Password: {raw_password}")
+        print(f"✅ Admin added. Name: {name}, Email: {email}, Password: {raw_password}")
